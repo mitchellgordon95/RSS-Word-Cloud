600b96f7cb4648af29b712ca4d0cf13a1651436e
Mitchell Gordon <mitchell.gordon95@gmail.com>
2019-01-30 14:52:46 -0500

    Just use titles, get rid of unused dependencies

--- rssSum.py
+++ rssSum.py
@@ -28,42 +28,13 @@ import sys
 FEED = raw_input("Enter the url of the feed: ")
 
 # Get all the urls:
-print "Retrieving rss feed..."
+print("Retrieving rss feed...")
 feedObj = feedparser.parse(FEED)
+titles = [item['title'] for item in feedObj['items']]
 
-webLinks = []
-for link in feedObj["items"]:
-    webLinks.append(link["link"])
-
-# Download the html pages and parse into text:
-print "Downloading and parsing pages..."
-allText = ""
-for i, link in enumerate(webLinks):
-    # Status Bar
-    sys.stdout.write("\r " + str(i*100/float(len(webLinks))) + "%")
-    sys.stdout.flush()
-
-    try:
-        # Download the link
-        response = urllib2.urlopen(link)
-        rawHtml = response.read()
-
-        # Parse the link
-        soup = BeautifulSoup(rawHtml, "html5lib")
-        paragraphs = soup.find_all('p')
-        rawText = ""
-        
-        for para in paragraphs:
-            rawText += para.getText()
-        rawText = rawText.encode("ascii", 'ignore')
-        allText = rawText 
-
-    except (urllib2.HTTPError, ValueError):
-        print "Error on : ", link
-
-print "\nConstructing cloud..."
+print("\nConstructing cloud...")
 
 # Construct the word cloud
-wordcloud = WordCloud(width=800, height=400).generate(allText)
+wordcloud = WordCloud(width=800, height=400).generate(' '.join(titles))
 wordcloud.to_file("image.png")
-print "Done!"
+print("Done!")
